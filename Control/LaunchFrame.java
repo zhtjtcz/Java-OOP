77c86a5e50f9c7971523707451591f096de494f2
zhtjtcz <18377221@buaa.edu.cn>
2021-06-07 20:10:47 +0800

    quit button

--- Control/LaunchFrame.java
+++ Control/LaunchFrame.java
@@ -11,49 +11,66 @@ public class LaunchFrame extends JFrame {
 	public JLayeredPane layeredPane = new JLayeredPane();
 
 	public LaunchFrame() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Plants VS Zombies");
-        this.setSize(810, 625);
-        this.setResizable(false);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-        this.setContentPane(layeredPane);
-        this.setIconImage(new ImageIcon("img\\Icon.png").getImage());
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Plants VS Zombies");
+		this.setSize(810, 625);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		this.setContentPane(layeredPane);
+		this.setIconImage(new ImageIcon("img\\Icon.png").getImage());
 
 		ImageIcon background = new ImageIcon("img\\Surface.png");
-        JPanel panel = new JPanel() {
-            private static final long serialVersionUID = 1L;
+		JPanel panel = new JPanel() {
+			private static final long serialVersionUID = 1L;
 
-            public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(background.getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
-            }
-        };
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.drawImage(background.getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
+			}
+		};
 
 		panel.setBounds(0, 0, background.getIconWidth(), background.getIconHeight());
-        layeredPane.add(panel, 10);
+		layeredPane.add(panel, 10);
 
 		ImageIcon startImg = new ImageIcon("img\\SelectorScreen_Adventure_highlight.png");
-        ImageIcon startImg2 = new ImageIcon("img\\SelectorScreen_StartAdventure_Highlight.png");
-        JLabel startLabel = new JLabel(startImg);
-        startLabel.addMouseListener(new MouseAdapter() {
-            public void mouseEntered(MouseEvent e) {
-                startLabel.setIcon(startImg2);
-            }
+		ImageIcon startImg2 = new ImageIcon("img\\SelectorScreen_StartAdventure_Highlight.png");
+		JLabel startLabel = new JLabel(startImg);
+		startLabel.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				startLabel.setIcon(startImg2);
+			}
 
-            public void mouseExited(MouseEvent e) {
-                startLabel.setIcon(startImg);
-            }
+			public void mouseExited(MouseEvent e) {
+				startLabel.setIcon(startImg);
+			}
 
-            public void mouseClicked(MouseEvent e) {
-                layeredPane.setVisible(false);
+			public void mouseClicked(MouseEvent e) {
+				layeredPane.setVisible(false);
 				// TODO game
 			}
-        });
-        startLabel.setBounds(410, 70, startImg.getIconWidth(), startImg2.getIconHeight());
-        layeredPane.add(startLabel, 0);
+		});
+		startLabel.setBounds(410, 70, startImg.getIconWidth(), startImg2.getIconHeight());
+		layeredPane.add(startLabel, 0);
 
-		
-    }
+		ImageIcon quitImg = new ImageIcon("img\\SelectorScreen_Quit1.png");
+		ImageIcon quitImg2 = new ImageIcon("img\\SelectorScreen_Quit2.png");
+		JLabel quitLabel = new JLabel(quitImg);
+		quitLabel.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				quitLabel.setIcon(quitImg2);
+			}
+
+			public void mouseExited(MouseEvent e) {
+				quitLabel.setIcon(quitImg);
+			}
+
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		quitLabel.setBounds(725, 510, quitImg.getIconWidth(), quitImg2.getIconHeight());
+		layeredPane.add(quitLabel, 0);
+	}
 
 }
