6b389842f0bf5fe448c8c5954d1118c2c8a61a9d
zhtjtcz <18377221@buaa.edu.cn>
2021-06-07 20:07:55 +0800

    start game button

--- Control/LaunchFrame.java
+++ Control/LaunchFrame.java
@@ -32,6 +32,28 @@ public class LaunchFrame extends JFrame {
 
 		panel.setBounds(0, 0, background.getIconWidth(), background.getIconHeight());
         layeredPane.add(panel, 10);
+
+		ImageIcon startImg = new ImageIcon("img\\SelectorScreen_Adventure_highlight.png");
+        ImageIcon startImg2 = new ImageIcon("img\\SelectorScreen_StartAdventure_Highlight.png");
+        JLabel startLabel = new JLabel(startImg);
+        startLabel.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e) {
+                startLabel.setIcon(startImg2);
+            }
+
+            public void mouseExited(MouseEvent e) {
+                startLabel.setIcon(startImg);
+            }
+
+            public void mouseClicked(MouseEvent e) {
+                layeredPane.setVisible(false);
+				// TODO game
+			}
+        });
+        startLabel.setBounds(410, 70, startImg.getIconWidth(), startImg2.getIconHeight());
+        layeredPane.add(startLabel, 0);
+
+		
     }
 
 }
