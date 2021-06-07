c9cca5ebfc8db3897a29e01f03aae5a4fd43548b
zhtjtcz <18377221@buaa.edu.cn>
2021-06-07 21:18:09 +0800

    card

---
+++ Card/card.java
@@ -0,0 +1,35 @@
+package Card;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.*;
+
+public class card extends JLabel implements MouseListener, Runnable{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void run(){
+		return;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+}
