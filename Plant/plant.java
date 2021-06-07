83388c9a3fc44417b8c8c9ab82e30b2cb119ce66
zhtjtcz <18377221@buaa.edu.cn>
2021-06-07 19:28:29 +0800

    plant

---
+++ Plant/plant.java
@@ -0,0 +1,23 @@
+package Plant;
+
+import javax.swing.*;
+
+public class plant extends JLabel implements Runnable {
+	private String name;
+
+	protected int hp = 0;
+	private int prive = 0;
+	private int CD;
+	private int Cnow = 0;
+	//累计时间
+
+	protected int sleepTime;
+	private int cardCD;
+	private int state;
+
+
+	@Override
+	public void run(){
+		return;
+	}
+}
