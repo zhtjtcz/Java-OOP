4e18c9b1b758ab53f34cc71928a40bac74e3a260
zhtjtcz <18377221@buaa.edu.cn>
2021-06-08 10:32:18 +0800

    Card and Sun

--- Card/card.java
+++ Card/card.java
@@ -1,20 +1,56 @@
 package Card;
 
+import Plant.*;
+import Control.*;
+
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
 import javax.swing.*;
+import java.util.*;
 
 public class Card extends JLabel implements MouseListener, Runnable{
 	private static final long serialVersionUID = 1L;
+	private boolean inCooling;
+	private boolean isChoosed;
+	private int cd;
+	private int totTime;
+	// CD and cooling
 
-	@Override
-	public void run(){
-		return;
-	}
+	private String cardName;
+	private int price;
+	private int cardWidth;
+	private int cardHeight;
+
+	private ImageIcon card;
+	private ImageIcon cardLight;
+	private ImageIcon cardDark;
+	private ImageIcon cardCooling;
+
+	private ImageIcon preImg;
+	private ImageIcon blurImg;
+	private Rectangle rectangle;
+	private Controller controller;
+
+	private Map<String, Plant> plantMap;
+
+	private int index;
+
+	public int getIndex(){return index;}
+	public void setIndex(int index) {this.index = index;}
+	public void setRectangle(int x, int y, int w, int h){this.rectangle = new Rectangle(x, y, w, h);}
+	public Rectangle getRectangle(){return rectangle;}
+	public void setInCooling(boolean b) {this.inCooling = b;}
+	public boolean getInCooling() {return inCooling;}
+	public int getPrice() {return price;}
+	public String getCardName() {return cardName;}
+	public int getCardWidth() {return cardWidth;}
+	public int getCardHeight() {return cardHeight;}
+	public void setChoosed(boolean choosed) {this.isChoosed = choosed;}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		selected();
 	}
 
 	@Override
@@ -31,5 +67,98 @@ public class Card extends JLabel implements MouseListener, Runnable{
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
+	}
+
+	public Card(String name, Controller controller){
+		this.controller = controller;
+		this.plantMap = controller.getPlantMap();
+		this.cardName = name;
+		this.cardLight = new ImageIcon("Img\\Cards\\" + cardName + "0.png");
+		this.cardDark = new ImageIcon("Img\\Cards\\" + cardName + "1.png");
+		this.cardCooling = new ImageIcon("Img\\Cards\\" + cardName + "2.png");
+		this.preImg = new ImageIcon("img\\" + cardName + "\\" + cardName + "_0.png ");
+		this.blurImg = new ImageIcon("img\\Blurs\\" + cardName + ".png ");
+		this.card = cardDark;
+		this.inCooling = true;
+		this.addMouseListener(this);
+		this.cardHeight = cardLight.getIconHeight();
+		this.cardWidth = cardLight.getIconWidth();
+		this.price = plantMap.get(cardName).getPrice();
+		this.cd = plantMap.get(cardName).getCardCD();
+	}
+
+	public boolean sunCountEnough(int SunCount){
+		return SunCount >= plantMap.get(this.cardName).getPrice();
+	}
+
+	public void check(int SunCount){
+		if (sunCountEnough(SunCount) && !inCooling) {
+			card = cardLight;
+			this.repaint();
+		} else {
+			card = cardDark;
+			this.repaint();
+		}
+	}
+	// 能否选择这个卡，能：light，否：dark
+
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		int h = card.getIconHeight(), w = card.getIconWidth();
+		g.drawImage(card.getImage(), 0, 0, w, h, this);
+		if (inCooling){
+			g.drawImage(cardCooling.getImage(), 0, 0, w, h - h * totTime / cd, 0, 0, w, h - h * totTime / cd, this);
+		}
+	}
+
+	public void selected(){
+		if (!inCooling && controller.getIntSunCount() >= this.getPrice()){
+			controller.getTopPanel().setVisible(true);
+			controller.setPreImg(this.preImg);
+			controller.setBlurImg(this.blurImg);
+			controller.setCard(this);
+			controller.setSelectedIndex(index);
+			controller.nowPlant = new Plant().getPlant(this.getCardName());
+			inCooling = true;
+			isChoosed = true;
+			check(0);
+		}
+	}
+	// Select a card
+
+	@Override
+	public void run(){
+		while (!controller.isRunning){
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		while (controller.isRunning){
+			while (controller.isRunning && (!inCooling || (inCooling && isChoosed))){
+				check(controller.getIntSunCount());
+				try {
+					Thread.sleep(4);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			check(controller.getIntSunCount());
+			while (controller.isRunning){
+				try {
+					Thread.sleep(3);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				totTime += 3;
+				if (totTime >= cd) 	break;
+				
+				check(controller.getIntSunCount());
+			}
+			totTime = 0;
+			inCooling = false;
+		}
 	}
 }
---
+++ Sun/SunProducer.java
@@ -0,0 +1,40 @@
+package Sun;
+
+import Control.*;
+
+public class SunProducer implements Runnable {
+	private Controller controller;
+
+	public SunProducer(Controller controller) {
+		this.controller = controller;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		while (!controller.isRunning) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		while (controller.isRunning) {
+			try {
+				Thread.sleep((int) (Math.random() * 1000) + 4000);
+				// 4-5s随机生成一个
+				
+				new Thread(new Sun(controller)).start();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		Thread.currentThread().interrupt();
+	}
+}
