a56bd76c6095faac4d862255ac3f8fb57f947c73
zhtjtcz <18377221@buaa.edu.cn>
2021-06-10 20:38:01 +0800

    makemap

---
+++ src/Control/MapMaker.java
@@ -0,0 +1,26 @@
+package Control;
+
+public class MapMaker {
+    public MapMaker(){}
+
+    private String GetPlant(){
+        int x = (int) (Math.random() * 1000);
+        x %= 100;
+        if (x <= 23)    return "SunFlower";
+        else if (x<=75) return "PeaShooter";
+        else if (x<=87) return "Repeater";
+        else return "WallNut";
+        // 0~23  -> Sunflower
+        // 24~75 -> Shoot
+        // 76~87 -> Repeat
+        // 88~99 -> WallNut
+    }
+
+    public String [][] GetMap(int n, int m) {
+        String[][] ans = new String[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                ans[i][j] = GetPlant();
+        return ans;
+    }
+}
